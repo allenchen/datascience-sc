5b7e0751167d58820f9106e76cacaac6f77da6a1
Jonathan Ko <jonathan.ko@berkeley.edu>
2012-04-24 13:15:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatascienceScSite::Application.config.secret_token = '7d89a3d7b2ffc662f58e4a49e49b0b8ce57a619109e165b82274259c29647c1666e2f5aae7499415c9b0732c8d0bd7b9ef0bcc625fd05f39eafe815fee177dfd'
