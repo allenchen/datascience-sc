12cd2df9f4d61b7a89cb6d81e42f28eddc19442f
Jonathan Ko <jonathan.ko@berkeley.edu>
2012-04-25 15:30:40 -0700

    Got something working

--- webservice/app.js
+++ webservice/app.js
@@ -1,36 +1,27 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes');
+  , exec = require('child_process').exec
+  ;
 
 var app = module.exports = express.createServer();
 
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+  res.send('sup brah');
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.get('/predict', function(req, res){
+  p = {
+    stuff: [],
+  };
+
+  child = exec('./runner', function(error, stdout, stderr){
+    if(stderr) p.error = stderr;
+    if(error) p.error = error;
+    p.stuff = stdout;
+
+    res.send(p);
+  });
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000, function(){
+app.listen(9999, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
