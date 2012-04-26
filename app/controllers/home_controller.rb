d80a45b87c31419af8921611e8461e3938d2b8ef
Jonathan Ko <jonathan.ko@berkeley.edu>
2012-04-26 13:25:33 -0700

    ZOMG

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,19 @@
+require 'open-uri'
+
 class HomeController < ApplicationController
 
   def index
   end
 
   def tech
+  end
+
+  def predict
+  end
+
+  def predict_post
+    json = open("http://ec2-50-18-88-226.us-west-1.compute.amazonaws.com:9999/predict?date=2010-09-25&mid=383&pid=1614&prace=2&cid=2025&crace=2") {|io| io.read}
+    @json = ActiveSupport::JSON.decode(json)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ DatascienceScSite::Application.routes.draw do
 
   get  'tech' => 'home#tech', :as => :tech
 
+  get  'predict' => 'home#predict', :as => :predict
+  post 'predict' => 'home#predict_post'
+
   root :to => 'home#index', :as => :home
 
 end
